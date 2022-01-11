a7245050cd2d69bcf2b28600fda5b448bf7d8bf1
slawomir.rega <slawomir.rega@tlen.pl>
2022-01-11 11:12:13 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import React from "react";
+import ReactDom from "react-dom";
+
+function Greeting() {
+  return <h4>Greatting from react</h4>;
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
