5c95a43122606cecd24c65a6da047e24fdbd4de3
fazerus <48531049+fazerus@users.noreply.github.com>
2022-01-11 11:12:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function Greeting() {
-  return <h4>Greatting from react</h4>;
+  return <h4>Greatting from react and git</h4>;
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
